66ad62afb8d9a0e102270feffa6e24779756ef3c
INFABLE-7 <1460446@senati.pe>
2023-08-17 19:29:27 -0500

    puse una funcion para almacenar los numeros que pedimos en entrada, nota me faltaba

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -16,12 +16,26 @@ public class Mavenproject1 {
         System.out.println("#SISTEMA DE ORDEDNAR NUMEROS INGRESADOS AL AZAR");
         
         Scanner jean= new Scanner(System.in);
-        
+        int[] numeros = new int[5];
         System.out.println("INGRESE 5 NUMEROS AL AZAR: ");
         for (int i = 0; i < 5; i++) {
             System.out.print("Ingrese el número #" + (i + 1) + ": ");
             numeros[i] = jean.nextInt();
         }
         
+        int n = numeros.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    // Intercambiar elementos si están en el orden incorrecto
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+        
+                
+        }
+        
     }
 }
+    }
